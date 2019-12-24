2637f1f19a72c2d92d8318bbe44fc22c404e1faa
Seonghyeon Kim <kim@seonghyeon.dev>
2019-12-24 11:40:10 +0900

    Add plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,6 +25,7 @@ module.exports = {
     ],
   },
   plugins: [
+    'gatsby-plugin-sitemap',
     {
       resolve: "@narative/gatsby-theme-novela",
       options: {
@@ -48,6 +49,24 @@ module.exports = {
         theme_color: `#fff`,
         display: `standalone`,
         icon: `src/assets/favicon.png`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-143368612-1',
+        // Puts tracking script in the head instead of the body
+        head: true,
+        // IP anonymization for GDPR compliance
+        anonymize: true,
+        // Disable analytics for users with `Do Not Track` enabled
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ['/preview/**'],
+        // Specifies what percentage of users should be tracked
+        sampleRate: 100,
+        // Determines how often site speed tracking beacons will be sent
+        siteSpeedSampleRate: 10,
       },
     },
   ],
